8f16c2498b3dc85a0e597186502490242da8ebd7
André Silva <freaktsg@gmail.com>
2022-03-07 11:41:26 +0000

    adicionar o meu nome

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,14 +5,19 @@ public class HelloWorld extends JFrame {
 
     private JPanel panel;
     private JLabel lblHelloWorld;
+    private JLabel lblHelloWorld2;
 
     public HelloWorld(){
         panel = new JPanel(new GridLayout(1, 1));
-        lblHelloWorld = new JLabel("Hello World");
+        lblHelloWorld = new JLabel("Hello World  2");
+        lblHelloWorld2 = new JLabel("O andré esteve aqui");
         lblHelloWorld.setHorizontalAlignment(SwingConstants.CENTER);
+        lblHelloWorld2.setHorizontalAlignment(SwingConstants.CENTER);
 
         panel.add(lblHelloWorld);
+        panel.add(lblHelloWorld2);
         setContentPane(panel);
+
 
         setPreferredSize(new Dimension(600, 600));
         pack();
